de88b0277645f9d7d0b6cfc3941b6dbbab4b5434
MayHyeyeonKim <hkim7963@gmail.com>
2024-04-06 15:39:06 -0700

    completed ui with react

--- file-upload-system/src/App.js
+++ file-upload-system/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccessfully: false
+  }
+
+  onFileChange = event => {
+    this.setState({selectedFile: event.target.file[0]});
+  }
+
+  ononFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "demo file",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    )
+      //call api
+      console.log(formData);
+      this.setState({selectedFile: null});
+      this.setState({fileUploadedSuccessfully:true});
+  }
+
+  fileData = () => {
+    if (this.state.selectedFile){
+      return (
+        <div>
+        <h2>File Details:</h2>
+        <p>File Name: {this.state.selectedFile.name}</p>
+        <p>File Type: {this.state.selectedFile.type}</p>
+        <p>Last Modified: {" "}
+        {this.state.selectedFile.lastModifiedDate.toDateString()}</p>
+      </div>
+      )
+    }
+    else if (this.state.fileUploadedSuccessfully) {
+      return (
+        <div>
+          <br />
+          <h4>Your file has been successfully uploaded</h4>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose a file and then press the Upload button</h4>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return(
+      <div className="container">
+      <h2>Maynism File Upload System</h2>
+      <h3>File Upload with React and a a Serverless API!</h3>
+      <div>
+        <input type="file" onChange={this.onFileChange} />
+        <button onChange={this.onFileUpload}>Upload</button>
+      </div>
+      {this.fileData()}
     </div>
-  );
+    )
+    
+  }
+
 }
 
 export default App;
